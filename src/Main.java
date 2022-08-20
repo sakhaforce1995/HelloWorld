60d77f82eee3bef62b3e1691292cd9e420ad51f2
sakhaforce1995 <nurgun-andreev@mail.ru>
2022-08-20 18:07:01 +0900

    Начальный коммит

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main (String[] args){
+        int a = 1995;
+        int b = 27;
         System.out.println("Hello world");
+        System.out.println(a+b);
     }
 }
